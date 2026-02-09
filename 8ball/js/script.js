c657d303c18b040669433a96554340479732a6a5
notapyramid <tziesler@eastsideprep.org>
2026-02-09 14:55:26 -0800

    yayitworks

---
+++ 8ball/js/script.js
@@ -0,0 +1,18 @@
+function m8() {
+    var f = gf();
+    sf(f);
+}
+
+function gf() {
+    var f = ["yes", "no", "maybee", "6-7"];
+    var r = ran(f.length);
+    return f[r];
+}
+
+function ran(max){
+    return Math.floor(Math.random()*max);
+}
+
+function sf(f) {
+    document.getElementById("a").innerHTML = f;
+}
